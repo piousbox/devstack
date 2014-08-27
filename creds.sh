98faedbbc2190cb9f8646f46bcd956d22ee888a5
piousbox <piousbox@gmail.com>
2014-08-27 16:35:46 -0700

    fixin a config typo

--- creds.sh
+++ creds.sh
@@ -2,4 +2,4 @@
 
 export OS_AUTH_URL="http://10.3.30.165:35357/v2.0"
 export OS_SERVICE_TOKEN=nexenta
-export SEVICE_ENDPOINT="http://10.3.30.165:35357/v2.0"
+export OS_SERVICE_ENDPOINT="http://10.3.30.165:35357/v2.0"
