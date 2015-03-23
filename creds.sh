fc5ff7f1857190c992b8fb1e9779a4ef0997450d
Victor Pudeyev <piousbox@gmail.com>
2015-03-23 14:38:58 -0700

    adding creds file.

--- creds.sh
+++ creds.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-export OS_AUTH_URL="http://10.3.30.165:35357/v2.0"
+export OS_AUTH_URL="http://192.168.56.105:35357/v2.0"
 export OS_SERVICE_TOKEN=nexenta
-export OS_SERVICE_ENDPOINT="http://10.3.30.165:35357/v2.0"
+export OS_SERVICE_ENDPOINT="http://192.168.56.105.165:35357/v2.0"
