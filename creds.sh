900341a4dc8a20b5538c3fdfb025389764f8c333
Victor Piousbox <piousbox@gmail.com>
2015-04-28 22:38:09 -0700

    adding stuff

---
+++ creds.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+export OS_AUTH_URL="http://192.168.56.130:35357/v2.0"
+export OS_SERVICE_TOKEN=nexenta
+export OS_SERVICE_ENDPOINT="http://192.168.56.130:35357/v2.0"
+
+# for glance
+export OS_USERNAME=admin
+export OS_PASSWORD=nexenta
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
